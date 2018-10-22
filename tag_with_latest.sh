526ef41512e1c5c5181d911c6883b9fff9fc4171
Benoit Blanchon <github@benoitblanchon.fr>
2018-10-22 09:16:36 +0200

    Only push the new tag

--- tag_with_latest.sh
+++ tag_with_latest.sh
@@ -7,4 +7,4 @@ cd $(dirname "$0")
 TAGNAME=$(git ls-remote --heads https://pdfium.googlesource.com/pdfium.git | grep -ohP 'chromium/\d+' | tail -n1)
 git pull
 git tag "$TAGNAME"
-git push --tags
+git push origin "$TAGNAME"
