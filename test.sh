f53f72b21a32c5581409f1bc3d4f482201e098df
Christoffer Green <christoffer.green@gmail.com>
2021-09-12 07:01:30 +0200

    Add Arm 32-bit target
    Add support for building Arm 32-bit binaries within Linux.
    
    Closes #46

--- test.sh
+++ test.sh
@@ -5,6 +5,10 @@ set -ex
 export SOURCE_DIR="$PWD/example"
 export PDFium_DIR="$PWD/staging"
 
+# Travis overrides CC and CXX, so we had to use different names
+[ -n "$_CC" ] && export CC="$_CC"
+[ -n "$_CXX" ] && export CC="$_CXX"
+
 [ "$TARGET_CPU" != "" ] && export CMAKE_OSX_ARCHITECTURES="$TARGET_CPU"
 
 mkdir build
