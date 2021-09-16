a4fb4807d0d0bcdc06556912374059d61f34d3f2
Benoit Blanchon <git@benoitblanchon.fr>
2021-09-16 17:09:16 +0200

    Travis: upload artifacts

--- travis.sh
+++ travis.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# install artifacts uploader
+curl -sL https://raw.githubusercontent.com/travis-ci/artifacts/master/install | bash
+
 ./build.sh
 
 [ -n "$TARGET_CPU" ] || ./test.sh
+
+$HOME/bin/artifacts upload \
+	--target-paths "artifacts/$TRAVIS_BUILD_NUMBER/$TRAVIS_JOB_NUMBER"\
+	$(ls pdfium*.tgz | tr "\n" ":")
