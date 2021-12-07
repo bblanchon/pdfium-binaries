dfc200d1211e642adc9418962cd3e362a5f2259a
Benoit Blanchon <git@benoitblanchon.fr>
2021-12-07 10:04:42 +0100

    Test: pass variables to CMake command line instead of using environment variables

--- steps/08-test.sh
+++ steps/08-test.sh
@@ -2,49 +2,75 @@
 
 OS=${PDFium_TARGET_OS:?}
 CPU="${PDFium_TARGET_CPU:?}"
-PDFIUM="${PDFium_SOURCE_DIR:?}"
 SOURCE_DIR="$PWD/example"
+ANDROID_TOOLCHAIN="${PDFium_SOURCE_DIR:?}/third_party/android_ndk/toolchains/llvm/prebuilt/linux-x86_64/bin/"
+CMAKE_ARGS=()
 
 export PDFium_DIR="$PWD/staging"
-[ "$OS" == "win" ] && export CMAKE_GENERATOR="Visual Studio 16 2019"
+
+[ "$OS" == "android" ] && export PATH="$ANDROID_TOOLCHAIN:$PATH"
 
 case "$OS-$CPU" in
-  win-x86)    
-    export CMAKE_GENERATOR_PLATFORM="Win32"
-    ;;
-  win-x64)
-    export CMAKE_GENERATOR_PLATFORM="x64"
-    ;;
-  mac-arm64)
-    export CMAKE_OSX_ARCHITECTURES="arm64"
-    ;;
-  linux-arm)
-    export CC="arm-linux-gnueabihf-gcc" CXX="arm-linux-gnueabihf-g++"
-    ;;
-  linux-arm64)
-    export CC="aarch64-linux-gnu-gcc" CXX="aarch64-linux-gnu-g++"
-    ;;
   android-arm)
-    export PATH="$PDFIUM/third_party/android_ndk/toolchains/llvm/prebuilt/linux-x86_64/bin/:$PATH"
-    export CC="armv7a-linux-androideabi16-clang" CXX="armv7a-linux-androideabi16-clang++"
+    CMAKE_ARGS+=(
+      -D CMAKE_C_COMPILER="armv7a-linux-androideabi16-clang"
+      -D CMAKE_CXX_COMPILER="armv7a-linux-androideabi16-clang++"
+    )
     ;;
   android-arm64)
-    export PATH="$PDFIUM/third_party/android_ndk/toolchains/llvm/prebuilt/linux-x86_64/bin/:$PATH"
-    export CC="aarch64-linux-android21-clang" CXX="aarch64-linux-android21-clang++"
-    ;;
-  android-x86)
-    export PATH="$PDFIUM/third_party/android_ndk/toolchains/llvm/prebuilt/linux-x86_64/bin/:$PATH"
-    export CC="i686-linux-android16-clang" CXX="i686-linux-android16-clang++"
+    CMAKE_ARGS+=(
+      -D CMAKE_C_COMPILER="aarch64-linux-android21-clang"
+      -D CMAKE_CXX_COMPILER="aarch64-linux-android21-clang++"
+    )
     ;;
   android-x64)
-    export PATH="$PDFIUM/third_party/android_ndk/toolchains/llvm/prebuilt/linux-x86_64/bin/:$PATH"
-    export CC="x86_64-linux-android21-clang" CXX="x86_64-linux-android21-clang++"
+    CMAKE_ARGS+=(
+      -D CMAKE_C_COMPILER="x86_64-linux-android21-clang"
+      -D CMAKE_CXX_COMPILER="x86_64-linux-android21-clang++"
+    )
+    ;;
+  android-x86)
+    CMAKE_ARGS+=(
+      -D CMAKE_C_COMPILER="i686-linux-android16-clang"
+      -D CMAKE_CXX_COMPILER="i686-linux-android16-clang++"
+    )
+    ;;
+  linux-arm)
+    CMAKE_ARGS+=(
+      -D CMAKE_C_COMPILER="arm-linux-gnueabihf-gcc"
+      -D CMAKE_CXX_COMPILER="arm-linux-gnueabihf-g++"
+    )
+    ;;
+  linux-arm64)
+    CMAKE_ARGS+=(
+      -D CMAKE_C_COMPILER="aarch64-linux-gnu-gcc"
+      -D CMAKE_CXX_COMPILER="aarch64-linux-gnu-g++"
+    )
+    ;;
+  mac-arm64)
+    CMAKE_ARGS+=(
+      -D CMAKE_OSX_ARCHITECTURES="arm64"
+    )
+    ;;
+  win-x86)
+    CMAKE_ARGS+=(
+      -G "Visual Studio 16 2019"
+      -A Win32
+    )
+    ;;
+  win-x64)
+    CMAKE_ARGS+=(
+      -G "Visual Studio 16 2019"
+      -A x64
+    )
     ;;
 esac
 
+CMAKE_ARGS+=("$SOURCE_DIR")
+
 mkdir -p build
 cd build
-cmake "$SOURCE_DIR"
+cmake "${CMAKE_ARGS[@]}"
 cmake --build .
 
 if [ "$OS" == "win" ]; then
