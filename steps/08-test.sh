39cdee4b9201d1c6e6c15aa19e727adfd2856d14
Benoit Blanchon <git@benoitblanchon.fr>
2022-01-04 11:26:43 +0100

    Use nested case in test step

--- steps/08-test.sh
+++ steps/08-test.sh
@@ -11,90 +11,106 @@ export PDFium_DIR="$PWD/staging"
 case "$OS" in
   android)
     export PATH="$ANDROID_TOOLCHAIN:$PATH"
+    case "$CPU" in
+      arm)
+        CMAKE_ARGS+=(
+          -D CMAKE_C_COMPILER="armv7a-linux-androideabi16-clang"
+          -D CMAKE_CXX_COMPILER="armv7a-linux-androideabi16-clang++"
+        )
+        ;;
+      arm64)
+        CMAKE_ARGS+=(
+          -D CMAKE_C_COMPILER="aarch64-linux-android21-clang"
+          -D CMAKE_CXX_COMPILER="aarch64-linux-android21-clang++"
+        )
+        ;;
+      x64)
+        CMAKE_ARGS+=(
+          -D CMAKE_C_COMPILER="x86_64-linux-android21-clang"
+          -D CMAKE_CXX_COMPILER="x86_64-linux-android21-clang++"
+        )
+        ;;
+      x86)
+        CMAKE_ARGS+=(
+          -D CMAKE_C_COMPILER="i686-linux-android16-clang"
+          -D CMAKE_CXX_COMPILER="i686-linux-android16-clang++"
+        )
+        ;;
+    esac
     ;;
+
   ios)
-    # https://discourse.cmake.org/t/find-package-stops-working-when-cmake-system-name-ios/4609/7
     CMAKE_ARGS+=(
+      -D CMAKE_SYSTEM_NAME="iOS"
+      # https://discourse.cmake.org/t/find-package-stops-working-when-cmake-system-name-ios/4609/7
       -D CMAKE_FIND_ROOT_PATH_MODE_PACKAGE="BOTH"
       -D CMAKE_FIND_ROOT_PATH_MODE_INCLUDE="BOTH"
       -D CMAKE_FIND_ROOT_PATH_MODE_LIBRARY="BOTH"
     )
+    case "$CPU" in
+      arm64)
+        CMAKE_ARGS+=(
+          -D CMAKE_OSX_SYSROOT="iphoneos"
+          -D CMAKE_OSX_ARCHITECTURES="arm64"
+        )
+        ;;
+      x64)
+        CMAKE_ARGS+=(
+          -D CMAKE_OSX_SYSROOT="iphonesimulator"
+          -D CMAKE_OSX_ARCHITECTURES="x86_64"
+        )
+        ;;
+    esac
     ;;
-esac
 
-case "$OS-$CPU" in
-  android-arm)
-    CMAKE_ARGS+=(
-      -D CMAKE_C_COMPILER="armv7a-linux-androideabi16-clang"
-      -D CMAKE_CXX_COMPILER="armv7a-linux-androideabi16-clang++"
-    )
+  linux)
+    case "$CPU" in
+      arm)
+        CMAKE_ARGS+=(
+          -D CMAKE_C_COMPILER="arm-linux-gnueabihf-gcc"
+          -D CMAKE_CXX_COMPILER="arm-linux-gnueabihf-g++"
+        )
+        ;;
+      arm64)
+        CMAKE_ARGS+=(
+          -D CMAKE_C_COMPILER="aarch64-linux-gnu-gcc"
+          -D CMAKE_CXX_COMPILER="aarch64-linux-gnu-g++"
+        )
+        ;;
+    esac
     ;;
-  android-arm64)
-    CMAKE_ARGS+=(
-      -D CMAKE_C_COMPILER="aarch64-linux-android21-clang"
-      -D CMAKE_CXX_COMPILER="aarch64-linux-android21-clang++"
-    )
+
+  mac)
+    case "$CPU" in
+      arm64)
+        CMAKE_ARGS+=(
+          -D CMAKE_OSX_ARCHITECTURES="arm64"
+        )
+      ;;
+    esac
     ;;
-  android-x64)
-    CMAKE_ARGS+=(
-      -D CMAKE_C_COMPILER="x86_64-linux-android21-clang"
-      -D CMAKE_CXX_COMPILER="x86_64-linux-android21-clang++"
-    )
-    ;;
-  android-x86)
-    CMAKE_ARGS+=(
-      -D CMAKE_C_COMPILER="i686-linux-android16-clang"
-      -D CMAKE_CXX_COMPILER="i686-linux-android16-clang++"
-    )
-    ;;
-  ios-arm64)
-    CMAKE_ARGS+=(
-      -D CMAKE_SYSTEM_NAME="iOS"
-      -D CMAKE_OSX_SYSROOT="iphoneos"
-      -D CMAKE_OSX_ARCHITECTURES="arm64"
-    )
-    ;;
-  ios-x64)
-    CMAKE_ARGS+=(
-      -D CMAKE_SYSTEM_NAME="iOS"
-      -D CMAKE_OSX_SYSROOT="iphonesimulator"
-      -D CMAKE_OSX_ARCHITECTURES="x86_64"
-    )
-    ;;
-  linux-arm)
-    CMAKE_ARGS+=(
-      -D CMAKE_C_COMPILER="arm-linux-gnueabihf-gcc"
-      -D CMAKE_CXX_COMPILER="arm-linux-gnueabihf-g++"
-    )
-    ;;
-  linux-arm64)
-    CMAKE_ARGS+=(
-      -D CMAKE_C_COMPILER="aarch64-linux-gnu-gcc"
-      -D CMAKE_CXX_COMPILER="aarch64-linux-gnu-g++"
-    )
-    ;;
-  mac-arm64)
-    CMAKE_ARGS+=(
-      -D CMAKE_OSX_ARCHITECTURES="arm64"
-    )
-    ;;
-  win-arm64)
+
+  win)
     CMAKE_ARGS+=(
       -G "Visual Studio 16 2019"
-      -A ARM64
     )
-    ;;
-  win-x86)
-    CMAKE_ARGS+=(
-      -G "Visual Studio 16 2019"
-      -A Win32
-    )
-    ;;
-  win-x64)
-    CMAKE_ARGS+=(
-      -G "Visual Studio 16 2019"
-      -A x64
-    )
+    case "$CPU" in
+      arm64)
+        CMAKE_ARGS+=(
+          -A ARM64
+        )
+        ;;
+      x86)
+        CMAKE_ARGS+=(
+          -A Win32
+        )
+        ;;
+      x64)
+        CMAKE_ARGS+=(
+          -A x64
+        )
+        ;;
+    esac
     ;;
 esac
 
