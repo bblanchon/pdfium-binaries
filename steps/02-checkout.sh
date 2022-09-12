40baf343bf76d5b63c15dda77787c658fa497a5f
Benoit Blanchon <git@benoitblanchon.fr>
2022-09-12 19:04:24 +0200

    Checkout step: reset local copy so that patch can be reapplied

--- steps/02-checkout.sh
+++ steps/02-checkout.sh
@@ -15,4 +15,13 @@ fi
 gclient config --unmanaged "$PDFium_URL" "${CONFIG_ARGS[@]-}"
 echo "target_os = [ '$OS' ]" >> .gclient
 
+
+# Reset
+if [ -e 'pdfium' ]; then
+  for folder in pdfium pdfium/build pdfium/third_party/libjpeg_turbo; do
+    git -C $folder reset --hard
+    git -C $folder clean -df
+  done
+fi
+
 gclient sync -r "origin/${PDFium_BRANCH:-main}" --no-history --shallow
