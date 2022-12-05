1fc020b2d8fd7dc17459953c6f1cb450572114d2
Benoit Blanchon <git@benoitblanchon.fr>
2022-12-05 10:03:32 +0100

    Move patches from `build/config/BUILDCONFIG.gn` to `build_overrides/BUILDCONFIG.gn`
    https://pdfium.googlesource.com/pdfium/+/3836eb0

--- steps/03-patch.sh
+++ steps/03-patch.sh
@@ -14,7 +14,7 @@ git apply -v "$PATCHES/public_headers.patch"
 
 case "$OS" in
   android)
-    git -C build apply -v "$PATCHES/android/build.patch"
+    git apply -v "$PATCHES/android/pdfium.patch"
     ;;
 
   ios)
@@ -46,6 +46,7 @@ esac
 
 case "$TARGET_LIBC" in
   musl)
+    git apply -v "$PATCHES/musl/pdfium.patch"
     git -C build apply -v "$PATCHES/musl/build.patch"
     mkdir -p "build/toolchain/linux/musl"
     cp "$PATCHES/musl/toolchain.gn" "build/toolchain/linux/musl/BUILD.gn"
