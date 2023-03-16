c2ca13f90d0234352965519d0f22296689a7911f
Jeroen Bobbeldijk <jeroen@klippa.com>
2023-03-16 15:27:38 +0100

    WASM: Update emsdk (#103)

--- steps/01-install.sh
+++ steps/01-install.sh
@@ -70,8 +70,8 @@ case "$TARGET_OS" in
       git clone https://github.com/emscripten-core/emsdk.git
     fi
     pushd emsdk
-    ./emsdk install 2.0.24
-    ./emsdk activate 2.0.24
+    ./emsdk install 3.1.34
+    ./emsdk activate 3.1.34
     echo "$PWD/upstream/emscripten" >> "$PATH_FILE"
     echo "$PWD/upstream/bin" >> "$PATH_FILE"
     popd
