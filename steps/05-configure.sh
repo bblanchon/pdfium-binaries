87d254d512a1a681d43a903c897a5b4b08f12559
Kleis Auke Wolthuizen <github@kleisauke.nl>
2023-09-20 15:30:09 +0200

    Linux: set `use_allocator_shim` to `false`
    On most platforms, Chrome intercepts and reroutes the `malloc()` /
    `operator new` symbols (and corresponding `free()` / `delete` and
    other variants), using the allocator shim. Subsequently, the
    allocator shim typically directs these symbols to the
    PartitionAlloc memory allocator.
    
    Overriding these functions can lead to compatibility issues when
    being linked against other shared libraries, leading to unexpected
    behavior or crashes.
    
    Set `use_allocator_shim = false` to disable this behavior.
    
    See: #124.

--- steps/05-configure.sh
+++ steps/05-configure.sh
@@ -31,6 +31,9 @@ mkdir -p "$BUILD"
       echo "use_blink = true"
       [ "$ENABLE_V8" == "true" ] && [ "$TARGET_CPU" == "arm64" ] && echo 'arm_control_flow_integrity = "none"'
       ;;
+    linux)
+      echo 'use_allocator_shim = false'
+      ;;
     mac)
       echo 'mac_deployment_target = "10.13.0"'
       ;;
