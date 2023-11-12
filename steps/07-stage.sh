e334062c8d41aecc94578416f3ac45103553c9db
David Sungaila <david.sungaila@arcor.de>
2023-11-12 17:32:34 +0100

    Update workflow inputs

--- steps/07-stage.sh
+++ steps/07-stage.sh
@@ -38,7 +38,7 @@ case "$OS" in
     mv "$BUILD/pdfium.html" "$STAGING_LIB"
     mv "$BUILD/pdfium.js" "$STAGING_LIB"
     mv "$BUILD/pdfium.wasm" "$STAGING_LIB"
-    mv "$BUILD/obj/libpdfium.a" "$STAGING_OBJ/${emsdk_VERSION:3.1.12}"
+    mv "$BUILD/obj/libpdfium.a" "$STAGING_OBJ/${emsdk_VERSION:3.1.34}"
     rm -rf "$STAGING/include/cpp"
     rm "$STAGING/PDFiumConfig.cmake"
     ;;
