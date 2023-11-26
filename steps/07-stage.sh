7700c6f3dd413043857db47b5bc1921cf6eb5684
Benoit Blanchon <git@benoitblanchon.fr>
2023-11-26 08:55:23 +0100

    WASM: remove `libpdfium.a` from tarball and NuGet package
    Reverts 8d9cea2
    Reverts #123
    Closes #145

--- steps/07-stage.sh
+++ steps/07-stage.sh
@@ -37,7 +37,6 @@ case "$OS" in
     mv "$BUILD/pdfium.html" "$STAGING_LIB"
     mv "$BUILD/pdfium.js" "$STAGING_LIB"
     mv "$BUILD/pdfium.wasm" "$STAGING_LIB"
-    mv "$BUILD/obj/libpdfium.a" "$STAGING_LIB"
     rm -rf "$STAGING/include/cpp"
     rm "$STAGING/PDFiumConfig.cmake"
     ;;
