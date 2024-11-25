3d8a940c55f7fd8cb55fe35e468222bc6c2a5d15
Benoit Blanchon <git@benoitblanchon.fr>
2024-11-25 15:45:08 +0000

    Musl: set `use_custom_libcxx_for_host` to `false`
    We were getting error "Libc++ only supports GCC 14 and later" in `libc++`

--- steps/05-configure.sh
+++ steps/05-configure.sh
@@ -57,6 +57,7 @@ mkdir -p "$BUILD"
       echo 'is_musl = true'
       echo 'is_clang = false'
       echo 'use_custom_libcxx = false'
+      echo 'use_custom_libcxx_for_host = false'
       [ "$ENABLE_V8" == "true" ] && case "$TARGET_CPU" in
         arm)
             echo "v8_snapshot_toolchain = \"//build/toolchain/linux:clang_x86_v8_arm\""
